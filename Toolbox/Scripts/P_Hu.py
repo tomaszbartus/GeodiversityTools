ed8e9b7adf86e59badad1fd14e46c099ab3cbcde
Tomasz Bartuś <bartus@agh.edu.pl>
2025-11-26 21:45:57 +0100

    P_Hu.py modifications, new script P_Ne2/py

--- Toolbox/Scripts/P_Hu.py
+++ Toolbox/Scripts/P_Hu.py
@@ -1,4 +1,4 @@
-# Geodiversity Tool P_Hjed
+# Geodiversity Tool P_Hu
 # Calculates the number of geosites (point features) within each polygon of the analytical grid
 # Author: bartus@agh.edu.pl
 # 2025-11-26
@@ -27,19 +27,18 @@ try:
     # CHECK FOR EXISTING OUTPUT FIELDS (re-running protection)
     # ----------------------------------------------------------------------
     field_raw = f"{prefix}_H"
-    field_std = f"{prefix}_Ne_MM"
 
     existing_fields = [f.name for f in arcpy.ListFields(grid_fl)]
 
     if field_raw in existing_fields:
         arcpy.AddError(
             f"Fields '{field_raw}' already exist in the analytical grid.\n"
-            f"Remove these fields before re-running the P_H tool."
+            f"Remove these fields before re-running the P_Hu tool."
         )
         raise Exception("Existing output field detected. Aborting tool execution.")
 
     # ----------------------------------------------------------------------
-    # 1. CREATE TEMPORARY LAYER WITH COUNT FIELD
+    #
     # ----------------------------------------------------------------------
     intersect_fc = f"{workspace_gdb}\\{prefix}_Ne_Int"
     tabulate_intersection_table = f"{workspace_gdb}\\{prefix}_Nc_Tbl"
@@ -48,26 +47,26 @@ try:
     # ----------------------------------------------------------------------
     # 2. SPATIAL JOIN
     # ----------------------------------------------------------------------
-    # arcpy.analysis.SpatialJoin(target_features, join_features, out_feature_class, {join_operation}, {join_type}, {field_mapping}, {match_option}, {search_radius}, {distance_field_name}, {match_fields})
     arcpy.analysis.SpatialJoin(grid_fl, landscape_fl, intersect_fc, "JOIN_ONE_TO_ONE","KEEP_ALL", "", "INTERSECT")
 
     arcpy.management.AlterField(intersect_fc, "Join_Count", "Ne")
 
     # ----------------------------------------------------------------------
     # 3. TABULATE INTERSECTION
+    # Funkcja tworzy tabelę złożoną z kolumn: 1) ID komórki grida, 2) wartość kategorii, 3) liczby punktów tej kategorii (Nc)
     # ----------------------------------------------------------------------
     arcpy.analysis.TabulateIntersection(grid_fl, grid_id_field, landscape_fl, tabulate_intersection_table, landscape_attr)
 
     # ----------------------------------------------------------------------
-    # 4. qi = Nc / Ne
+    # 4. q_i = Nc / Ne
     # ----------------------------------------------------------------------
-    # Add Ne attribute to the tabulate_intersection_table
+    # Add new attribute Ne to the tabulate_intersection_table
     arcpy.management.JoinField(tabulate_intersection_table, grid_id_field, intersect_fc, grid_id_field,["Ne"])
 
-    # Add new attribute qi to the  tabulate_intersection_table
+    # Add new attribute q_i to the tabulate_intersection_table
     arcpy.management.AddField(tabulate_intersection_table, "qi", "DOUBLE")
 
-    # Calculate qi
+    # Calculate q_i
     arcpy.management.CalculateField(tabulate_intersection_table,"qi","(!POINTS! / !Ne!) if !Ne! > 0 else 0","PYTHON3")
 
     # ----------------------------------------------------------------------
@@ -77,9 +76,9 @@ try:
     arcpy.management.CalculateField(tabulate_intersection_table,"H_i","-(!qi! * math.log(!qi!)) if !qi! > 0 else 0","PYTHON3")
 
     # ----------------------------------------------------------------------
-    # 6. SUMMARIZE ALL qi
+    # 6. SUMMARIZE ALL q_i
     # ----------------------------------------------------------------------
-    arcpy.analysis.Statistics(tabulate_intersection_table, H_table,[["H_i", "SUM"]], grid_id_field)
+    arcpy.analysis.Statistics(tabulate_intersection_table, H_table,["H_i", "SUM"], grid_id_field)
 
     # ----------------------------------------------------------------------
     # 7. Join back to grid layer
@@ -89,7 +88,7 @@ try:
     # ----------------------------------------------------------------------
     # 8. Rename joined fields
     # ----------------------------------------------------------------------
-    arcpy.management.AlterField(grid_fl, "SUM_H_i", f"{prefix}_H", landscape_attr + "_H")
+    arcpy.management.AlterField(grid_fl,"SUM_H_i", f"{prefix}_Hu", landscape_attr + "_Hu")
     arcpy.AddMessage(f"Unit entropy ({prefix}_H) calculated successfully.")
 
     # ----------------------------------------------------------------------
@@ -101,7 +100,6 @@ try:
     arcpy.management.Compact(workspace_gdb)
 
     arcpy.AddMessage("Ne calculation completed successfully.")
-
 
 except arcpy.ExecuteError:
     arcpy.AddError("Geoprocessing error occurred:")
